1252d7b2e5b4490664a4138ce316325600bb6116
Carlos Alberto <alberttcarlosu.u@gmail.com>
2026-05-09 17:36:13 -0300

    Novos progressos
    Co-authored-by: Guivini12 <Guivini12@users.noreply.github.com>

--- assets/js/ms.js
+++ assets/js/ms.js
@@ -1,8 +1,18 @@
-let carrinho = document.querySelector(".carrinho");
-let cartButton = document.querySelector("#cart");
+let carrinhoPainel = document.querySelector('.carrinho');
+let loginPainel = document.querySelector('.login-form');
 
-if (cartButton && carrinho) {
-    cartButton.onclick = () => {
-        carrinho.classList.toggle('active');
-    }
-}
+document.querySelector('#cart').onclick = () => {
+    carrinhoPainel.classList.toggle('active');
+    
+    loginPainel.classList.remove('active');
+};
+
+document.querySelector('#login').onclick = () => {
+    loginPainel.classList.toggle('active');
+    carrinhoPainel.classList.remove('active');
+};
+
+window.onscroll = () => {
+    carrinhoPainel.classList.remove('active');
+    loginPainel.classList.remove('active');
+};
